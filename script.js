6ec5f5ea98eec36d7b7c31887a0068a85341fe39
unknown <wardle2106@gmail.com>
2018-06-27 19:08:57 -0400

    update

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-//**********USE LINTER WHILE WRITING JS ON THIS PRJECT************//
-
-//NEED TO USE/ DOWNLOAD "ES LINT"******************
 
 //SETTING UP CLICK EVENT
-let button = document.getElementById('convert-button');
+const BUTTON = document.getElementById('convert-button');
 
-button.addEventListener('click', function(){
-
-//***********NEED TO ADD ENTER KEYPRESS EVENT*******************
+BUTTON.addEventListener('click', function(){
 
 
-
-  //DISPLAYING CURRENCIES
+  //ASYNC/AWAIT TO GET JSON 
 
   async function getData() {
 
@@ -40,32 +34,48 @@ button.addEventListener('click', function(){
     //SHOULD BE ABLE TO USE THIS INDEX TO RREPLACE RATE IN ARRAY ALSO***********
     let arrayIndex = currencyType.indexOf(abbreviation);
     currencyType.splice(arrayIndex, 1, 'EUR');
-    //currencyType.sort();
 
+    let unsortedIndex = currencyType.indexOf('EUR');
+    currencyType.sort();
 
     //CREATING NEW rates ARRAY
     let newBaseAmount = rates[arrayIndex];
     let newRates = [];
     rates.splice(arrayIndex, 1, 1);
 
+    //DOING MATH AND PUSHING TO NEW ARRAY
     rates.forEach(cur => newRates.push(cur / newBaseAmount));
-    console.log(newRates);
+
+    //SORTING RATES TO MATCH ORDER OF currencyType ARRAY
+
+    let sortedIndex = currencyType.indexOf('EUR');
+
+    let euroRate = newRates[unsortedIndex];
+    newRates.splice(unsortedIndex, 0);
+    newRates.splice(sortedIndex, 1, euroRate);
+
     for (let i = 0; i <= 5; i++) {
       document.getElementById(`currency${i}`).style.display = 'block';
       document.getElementById(`display-amount${i}`).innerText = '$' + Math.round(newRates[i] * amount * 100) / 100;
       document.getElementById(`display-currency-type${i}`).innerText = currencyType[i];
     }
-    //NEED TO DO MATH TO THE RATES ARRAY AND TO SELECTED CURRENCY
   }
 }
 
   getData();
 
-
 });
 
 
+//ADDING ENTER KEYPRESS EVENT
+ const INPUT = document.getElementById('amount-converted');
 
+INPUT.addEventListener('keypress', function(event){
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    BUTTON.click();
+  }
+});
 
 
 
@@ -78,7 +88,7 @@ buttonReset.addEventListener('click', function(){
   for (let i = 0; i <= 5; i++) {
     document.getElementById('currency' + [i]).style.display = 'none';
     document.getElementById('amount-converted').value = '';
-
   }
+
 
 });
